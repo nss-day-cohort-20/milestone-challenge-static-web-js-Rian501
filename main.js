4c1831d27f6f2e817cc8db9e4fe45336bcbbcbb8
Rian501 <eclaire.lemmon@gmail.com>
2017-06-13 16:04:38 -0500

    Initial files

---
+++ main.js
@@ -0,0 +1,7 @@
+let howTall = document.getElementById('getNumber').value;
+let whatChar = document.getElementById('getChar').value;
+let activateBtn = document.getElementById('growBtn');
+
+console.log("How tall and what char?", howTall, whatChar);
+
+//look at overly excited plus temp converter to review how those work
