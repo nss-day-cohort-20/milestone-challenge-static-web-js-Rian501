e2fcb1cd3c9e46e60f75504503fbf24bef52b2a0
Rian501 <eclaire.lemmon@gmail.com>
2017-06-15 17:40:34 -0500

    Tree builds with improper spacing but correct number of chars

--- main.js
+++ main.js
@@ -26,5 +26,22 @@ console.log("specs first", specs);
 function getInfo (specs) {
 	 specs.height = parseInt(document.getElementById('getNumber').value);
 	 specs.char = document.getElementById('getChar').value;
+	if (document.getElementById('getNumber').value === '') {	window.alert("Both fields require an appropriate value.");
+	} else if (specs.char === "") {
+		window.alert("Both fields require an appropriate value.");
+	}
 	console.log("specs?", specs.char, specs.height);
+	growTree(specs);
+}
+
+function growTree(specs) {
+	let emptyTree = '';
+	let spaces = '';
+	let doubleChar = specs.char + specs.char;
+	for (i=0; i<1; i++) {
+		console.log(emptyTree += specs.char);
+	};
+	for (i=0; i<specs.height; i++) {
+		console.log(emptyTree+= doubleChar);
+	}
 }
