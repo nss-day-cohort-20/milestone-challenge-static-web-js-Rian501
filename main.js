5d6e2e4fafa55ce476a2a741190a5b791eda766c
Rian501 <eclaire.lemmon@gmail.com>
2017-06-16 10:05:52 -0500

    Use .repeat to make the spaces stick

--- main.js
+++ main.js
@@ -2,12 +2,13 @@ let activateBtn = document.getElementById('growBtn');
 let numberField = document.getElementById('getNumber');
 let charField = document.getElementById('getChar');
 
-
+//start with 
 let specs = {
 	height: 0,
 	char: " "
 };
 
+//listen for user input three ways
 activateBtn.addEventListener("click", getInfo);
 numberField.addEventListener("keyup", function (event) {
 	if (event.key==="Enter") {
@@ -20,33 +21,29 @@ charField.addEventListener("keyup", function (event) {
 	}
 })
 
-console.log("specs first", specs);
 
-
+//get the input from the user and throw an alert if not right type of input
 function getInfo (specs) {
 	 specs.height = parseInt(document.getElementById('getNumber').value);
 	 specs.char = document.getElementById('getChar').value;
-	if (document.getElementById('getNumber').value === '') {	window.alert("Both fields require an appropriate value.");
+	if (document.getElementById('getNumber').value === '') {	
+		window.alert("Both fields require an appropriate value.");
 	} else if (specs.char === "") {
 		window.alert("Both fields require an appropriate value.");
 	}
-	console.log("specs?", specs.char, specs.height);
+console.log("Your tree is this tall:", specs.height);
+console.log("Your tree is made of:", specs.char);
 	growTree(specs);
 }
 
+//user input implemented to grow the tree
 function growTree(specs) {
-	let emptyTree = '';
-	let spaces = 'm';
-	for (i=2; i<specs.height; i++) {
-		spaces += 'm';
-	}
-//I need the number of spaces to start out at specs.height-1, then to decrease by one each time through the loop until they are at 0 and the tree is full.
+	let growingTree = specs.char;
+	let spaces = '';
 	let doubleChar = specs.char + specs.char;
-	for (i=0; i<1; i++) {
-		console.log(emptyTree += spaces + specs.char);
-	};
 	for (i=0; i<specs.height; i++) {
-		spaces -= spaces;
-		console.log(emptyTree+= spaces + doubleChar);
+		spaces = ' '.repeat(specs.height-1-i);
+		console.log(spaces + growingTree);
+		growingTree += doubleChar;
 	}
 }
